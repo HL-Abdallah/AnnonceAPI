c8414c0cb24d7058242115214c26e234f50d2b57
Abdallah Hameur lain <abdallah.hameurlain@eadn.dz>
2023-06-22 16:39:33 +0100

    add Swagger metadata

--- src/main/java/com/example/annonceapi/AnnonceApiApplication.java
+++ src/main/java/com/example/annonceapi/AnnonceApiApplication.java
@@ -1,13 +1,39 @@
 package com.example.annonceapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Annonces API Documentation",
+                description = "les docs sont générés automatiquement",
+                contact = @Contact(
+                        name = "Abdallah Hameur Lain",
+                        email = "hameurlain.abdallah@gmail.com",
+                        url = "https://www.linkedin.com/in/hameurlain-abdallah/"
+                ),
+                version = "1.0.alpha"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8082",
+                        description = "local : Intellij IDEA"
+                ),
+                @Server(
+                        url = "http://aws.someurl.com",
+                        description = "Remote docker thing"
+                )
+        }
+)
 public class AnnonceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnnonceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnnonceApiApplication.class, args);
+    }
 
 }
